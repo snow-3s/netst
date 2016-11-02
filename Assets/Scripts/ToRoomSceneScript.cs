dcaff2b466d638fe11386670a2c2c79674e85ac2
Manabe Shinya <manabeshinya@Manabe-no-MacBook-Air-2.local>
2016-11-02 14:25:33 +0900

    homeからroomへの画面遷移を実装

---
+++ Assets/Scripts/ToRoomSceneScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToRoomSceneScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void ButtonOnClick () {
+		Debug.Log("hello world");
+		Application.LoadLevel("room");
+	}
+
+}

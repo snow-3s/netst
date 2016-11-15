657fe092335afaa3e657a581d1e556678eeda010
Tomoaki FUJISHIMA <snow@club.kyutech.ac.jp>
2016-11-16 07:28:45 +0900

    端末の傾きでアバターの向きが変わるように

---
+++ Assets/Scripts/AvatarRotationUpdater.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AvatarRotationUpdater : Photon.MonoBehaviour {
+
+    GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+        if (photonView.isMine)
+        {
+            camera = GameObject.FindWithTag("MainCamera");
+        }
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if (photonView.isMine)
+        {
+            this.transform.rotation = Quaternion.Euler(0, camera.transform.eulerAngles.y, 0);
+        }
+	}
+
+}

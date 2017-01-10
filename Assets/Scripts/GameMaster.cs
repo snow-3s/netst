78a00235122d3921f499b321e199bdf8e8555c34
Tomoaki FUJISHIMA <fujishima@leo10.cse.kyutech.ac.jp>
2017-01-11 00:10:09 +0900

    ゲームの進行役クラスを作成、役職抽選を行うように

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : Photon.MonoBehaviour
+{
+    //参加者リスト
+    Dictionary<int, Participant> participants;
+
+    void Start()
+    {
+        //参加者確定、これ以上人が入れないように
+
+        //参加者リストを取得
+        PhotonPlayer[] players = PhotonNetwork.playerList;
+        //参加者抽選
+        participants = RoleSelecter.Run(players);
+        //役職通知
+        
+    }
+
+    void Update()
+    {
+
+    }
+}

ce2e089ed4fd59cafb0329631617c6b80e9cb9d8
Tomoaki FUJISHIMA <fujishima@leo10.cse.kyutech.ac.jp>
2017-01-13 06:46:49 +0900

    フェーズが切り替わるように+役職通知方法を変更

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -1,33 +1,156 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class GameMaster : Photon.MonoBehaviour
 {
+    public enum Phase
+    {
+        START, MORNING, DAYTIME, EVENING, NIGHT, HUMAN_VICTORY, WERWOLF_VICTORY, END
+    };
+    Phase currentPhase, nextPhase;
+
     //参加者リスト
     Dictionary<int, Participant> participants;
-
     void Start()
     {
         //参加者確定、これ以上人が入れないように
 
         //参加者リストを取得
         PhotonPlayer[] players = PhotonNetwork.playerList;
-        //参加者抽選
-        participants = RoleSelecter.Run(players);
-        //役職通知
-        foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("Player"))
+        participants = new Dictionary<int, Participant>();
+
+        currentPhase = Phase.END;
+        nextPhase = Phase.END;
+        StartCoroutine(Delay());
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(5);
+        //投票結果を通知
+        if (PhotonNetwork.isMasterClient)
         {
-            int playerId = gameObject.GetComponent<PhotonView>().ownerId;
-            gameObject.GetComponent<PlayerParameter>().SetRole(participants[playerId].GetRole());
+            //役職抽選
+            participants = RoleSelecter.Run(PhotonNetwork.playerList);
+            //役職通知&共有
+            foreach (Participant participant in participants.Values)
+            {
+                photonView.RPC("NotifyRole", PhotonTargets.All, participant);
+            }
         }
-        //ゲームマスター削除,Starter 生成
-        PhotonNetwork.InstantiateSceneObject("Prefabs/GameStarter", new Vector3(0, 2.5f, 0), Quaternion.identity, 0, null);
-        PhotonNetwork.Destroy(gameObject);
+        nextPhase = Phase.START;
     }
 
     void Update()
     {
+        if (currentPhase != nextPhase)
+        {
+            currentPhase = nextPhase;
+            switch (currentPhase)
+            {
+                case Phase.START:
+                    //開始時の演出
+                    PhotonNetwork.InstantiateSceneObject("Prefabs/Phases/DaytimePhase", new Vector3(0, 0, 0), Quaternion.identity, 0, null);
+                    break;
+
+                case Phase.MORNING:
+                    PhotonNetwork.InstantiateSceneObject("Prefabs/Phases/DaytimePhase", new Vector3(0, 0, 0), Quaternion.identity, 0, null);
+                    break;
+
+                case Phase.DAYTIME:
+                    PhotonNetwork.InstantiateSceneObject("Prefabs/Phases/DaytimePhase", new Vector3(0, 0, 0), Quaternion.identity, 0, null);
+                    break;
+
+                case Phase.EVENING:
+                    PhotonNetwork.InstantiateSceneObject("Prefabs/Phases/EveningPhase", new Vector3(0, 0, 0), Quaternion.identity, 0, null);
+                    break;
+
+                case Phase.NIGHT:
+                    PhotonNetwork.InstantiateSceneObject("Prefabs/Phases/NightPhase", new Vector3(0, 0, 0), Quaternion.identity, 0, null);
+                    break;
+
+                case Phase.HUMAN_VICTORY:
+                    PhotonNetwork.InstantiateSceneObject("Prefabs/Phases/DaytimePhase", new Vector3(0, 0, 0), Quaternion.identity, 0, null);
+                    break;
+
+                case Phase.WERWOLF_VICTORY:
+                    PhotonNetwork.InstantiateSceneObject("Prefabs/Phases/DaytimePhase", new Vector3(0, 0, 0), Quaternion.identity, 0, null);
+                    break;
+
+                case Phase.END:
+                    //ゲームマスター削除,Starter 生成
+                    PhotonNetwork.InstantiateSceneObject("Prefabs/GameStarter", new Vector3(0, 2.5f, 0), Quaternion.identity, 0, null);
+                    PhotonNetwork.Destroy(gameObject);
+                    break;
+            }
+        }
 
     }
+
+    public Participant[] GetParticipants()
+    {
+        return participants.Values.ToArray();
+    }
+
+    public void KillPlayer(int playerId)
+    {
+        participants[playerId].die();
+    }
+
+    //役職の通知&共有
+    [PunRPC]
+    void NotifyRole(Participant participant)
+    {
+        if(!PhotonNetwork.isMasterClient)
+            participants[participant.GetPlayerId()] = participant;
+        foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            if (participant.GetPlayerId() == gameObject.GetComponent<PhotonView>().ownerId)
+            {
+                gameObject.GetComponent<PlayerParameter>().SetRole(participant.GetRole());
+            }
+        }
+    }
+
+    //Phase 更新
+    public void EndPhase()
+    {
+        switch (currentPhase)
+        {
+            case Phase.START:
+                nextPhase = Phase.DAYTIME;
+                break;
+
+            case Phase.MORNING:
+                nextPhase = Phase.DAYTIME;
+                break;
+
+            case Phase.DAYTIME:
+                nextPhase = Phase.EVENING;
+                break;
+
+            case Phase.EVENING:
+                nextPhase = Phase.NIGHT;
+                break;
+
+            case Phase.NIGHT:
+                //勝敗判定
+                if (true)
+                    nextPhase = Phase.HUMAN_VICTORY;
+                else
+                    nextPhase = Phase.MORNING;
+                break;
+
+            case Phase.HUMAN_VICTORY:
+                nextPhase = Phase.END;
+                break;
+
+            case Phase.WERWOLF_VICTORY:
+                nextPhase = Phase.END;
+                break;
+        }
+    }
+
 }

b8f2a8dfa426ee2fe7532c5394bf4e207b8c843f
Tomoaki FUJISHIMA <fujishima@leo10.cse.kyutech.ac.jp>
2017-01-13 06:41:59 +0900

    昼,夕,夜フェーズの作成

---
+++ Assets/Scripts/Phases/DaytimePhase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaytimePhase : Photon.MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(DaytimeEnd());
+	}
+
+    IEnumerator DaytimeEnd()
+    {
+        yield return new WaitForSeconds(5);
+        GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>().EndPhase();
+        PhotonNetwork.Destroy(gameObject);
+    }
+	
+}

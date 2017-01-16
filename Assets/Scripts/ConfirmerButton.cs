47b88af70095b8602efe621e6c489c470196d59c
Tomoaki FUJISHIMA <fujishima@leo10.cse.kyutech.ac.jp>
2017-01-16 15:29:51 +0900

    Confirmer追加

---
+++ Assets/Scripts/ConfirmerButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfirmerButton : MonoBehaviour {
+
+    onConfirm callback = null;
+    
+    public delegate void onConfirm();
+
+    public void SetCallback(onConfirm callback)
+    {
+        this.callback = callback;
+    }
+
+    public void OnGazeEnter()
+    {
+        if (callback != null)
+        {
+            callback();
+        }
+        Destroy(transform.root.root.gameObject);
+    }
+
+}

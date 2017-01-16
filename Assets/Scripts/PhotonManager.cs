58ae7948f7e7f1a0252abe849831683e1f9bf004
Tomoaki FUJISHIMA <fujishima@leo10.cse.kyutech.ac.jp>
2017-01-17 03:45:52 +0900

    ランダムでアバターを生成するように

--- Assets/Scripts/PhotonManager.cs
+++ Assets/Scripts/PhotonManager.cs
@@ -9,6 +9,7 @@ public class PhotonManager : Photon.MonoBehaviour
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings("v0.1");
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
@@ -33,7 +34,9 @@ public class PhotonManager : Photon.MonoBehaviour
 		int angle = 30;
         GameObject prefabCamera = (GameObject)Resources.Load("Prefabs/vrcamera");
         GameObject camera = (GameObject)Instantiate(prefabCamera, new Vector3(), Quaternion.identity);
-		GameObject player = PhotonNetwork.Instantiate("Prefabs/Avatars/unitychan", new Vector3(3 * Mathf.Sin(angle * count * (Mathf.PI / 180)), 0.1f, 3 * Mathf.Cos(angle * count * (Mathf.PI / 180))), Quaternion.identity, 0);
+        string[] avatarArray = { "man", "old_man", "old_man2", "old_woman" };
+        string playerAvatarName = avatarArray[UnityEngine.Random.Range(0, avatarArray.Length)];
+		GameObject player = PhotonNetwork.Instantiate("Prefabs/Avatars/"+playerAvatarName, new Vector3(3 * Mathf.Sin(angle * count * (Mathf.PI / 180)), 0.1f, 3 * Mathf.Cos(angle * count * (Mathf.PI / 180))), Quaternion.identity, 0);
         GameObject head = player.transform.FindChild("head").gameObject;
         camera.GetComponent<PositionSynchronizer>().SetSyncTarget(head);
         //GameStarter generate as SceneObject

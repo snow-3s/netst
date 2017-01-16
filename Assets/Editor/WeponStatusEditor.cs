f49730818fbd4d57ba199ec7d59a8a77c9490582
Tomoaki FUJISHIMA <fujishima@leo10.cse.kyutech.ac.jp>
2017-01-17 01:08:09 +0900

    Voxelキャラ追加

---
+++ Assets/Editor/WeponStatusEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WeponStatus))]
+public class WeponStatusEditor : Editor {
+
+	private static readonly string[] DisplayedType = {"None","One hand wepon","Two hand wepon","Dual wepon","Pole wepon","bow"};
+	private static readonly int[] OptionType       = { 0, 1, 2, 3, 4, 5};
+	public override void OnInspectorGUI()
+	{
+		WeponStatus Wepon_Status = target as WeponStatus;
+
+		Wepon_Status.WeponType = EditorGUILayout.IntPopup ("Type",Wepon_Status.WeponType,DisplayedType,OptionType);
+	}
+}

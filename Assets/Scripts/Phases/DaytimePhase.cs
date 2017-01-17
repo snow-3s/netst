55ac992d55861d31c9c0f9bfb0898ffee17df4bc
Tomoaki FUJISHIMA <fujishima@leo10.cse.kyutech.ac.jp>
2017-01-18 05:51:29 +0900

    制限時間超過・過半数の人が希望すれば昼が終わるように

--- Assets/Scripts/Phases/DaytimePhase.cs
+++ Assets/Scripts/Phases/DaytimePhase.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class DaytimePhase : Photon.MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    bool isInterrupted = false;
+    int frame = 0;
+    int maxFrame = 60 * 60 * 5;
+
+    // Use this for initialization
+    void Start () {
         StartCoroutine(DaytimeEnd());
-	}
+        if (PhotonNetwork.isMasterClient)
+        {
+            GameObject obj = PhotonNetwork.InstantiateSceneObject("Prefabs/GazeTrigger", new Vector3(0, 3f, 0), Quaternion.identity, 0, null);
+            obj.GetComponent<GazeTrigger>().SetCallback(()=> { isInterrupted = true; });
+        }
+    }
+
+    void Update()
+    {
+        frame++;
+    }
 
     IEnumerator DaytimeEnd()
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitUntil(() => frame >= maxFrame || isInterrupted);
         GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>().EndPhase();
+        if (!isInterrupted)
+        {
+            PhotonNetwork.Destroy(GameObject.FindGameObjectWithTag("GazeTrigger"));
+        }
         PhotonNetwork.Destroy(gameObject);
     }
 	

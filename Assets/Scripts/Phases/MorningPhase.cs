0c81b49897a1ffd15f06d8b4985a5603203307f4
aoiro_makoto <aoiro_makoto@klab.ai.kyutech.ac.jp>
2017-01-18 13:31:05 +0900

    MorningPhase乗ってなかったみたいなので

--- Assets/Scripts/Phases/MorningPhase.cs
+++ Assets/Scripts/Phases/MorningPhase.cs
@@ -6,11 +6,47 @@ public class MorningPhase : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		telleePlayerId = 0;
+		PhotonNetwork.InstantiateSceneObject("Prefabs/Voter", new Vector3(0, 0, 0), Quaternion.identity, 0, null);
+		GameObject.FindGameObjectWithTag("Voter").GetComponent<Voter>().Run("fortuneTeller", "TelleeRoleNotify");
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
 	}
+
+	[PunRPC]
+	public void TelleeRoleNotify(int playerId)
+	{
+		Dictionary<int, Participant> participants = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>().GetParticipantsDictionary();
+		GameMaster gameMaster = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>();
+		GameObject prefabConfirmer = (GameObject)Resources.Load("Prefabs/Confirmer");
+		GameObject confirmer = Instantiate(prefabConfirmer, new Vector3(), Quaternion.identity);
+
+		if (playerId != 0)
+		{
+			if (GameObject.FindGameObjectWithTag ("GameMaster").GetComponent<GameMaster> ().GetParticipantsDictionary () [PhotonNetwork.player.ID].isSurvive ()) {
+				foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Player")) {
+					if (PhotonNetwork.player.GetRole () == "fortuneTeller") {
+						if (Participants [playerId].GetRole ().Equals ("warewolf")) {    //白黒通知
+							confirmer.GetComponent<Confirmer> ().SetText ("Player" + playerId + ": " + "black");
+							confirmer.GetComponent<Confirmer> ().SetCallback (() => {
+								Debug.Log (Participants [playerId].GetRole ());
+							});
+						} else {
+							confirmer.GetComponent<Confirmer> ().SetText ("Player" + playerId + ": " + "white");
+							confirmer.GetComponent<Confirmer> ().SetCallback (() => {
+								Debug.Log (Participants [playerId].GetRole ());
+							});
+						}
+					}
+				}
+			}
+		}
+		gameMaster.EndPhase();
+		PhotonNetwork.Destroy(gameObject);
+	}
+
+
+
 }
